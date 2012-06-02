babafb3f93ff30fa7f25a878a8eed43f9879fa2b
Thorsten Ball <mrnugget@gmail.com>
2012-06-02 13:57:57 +0200

    Add basic, hacky functionality to index page

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,30 @@
 $(function () {
-  console.log("Well, oh yeah!");
+  $('#search_movie').submit(function(){
+    var $form     = $(this);
+    var movieName = $form.children('#movie_name_input').val();
+    var movieYear = $form.children('#movie_year_input').val();
+
+    var apiUrl    = $form.attr('action') + "/" + movieYear + "/" + movieName;
+
+    $.get(apiUrl, function(movie){
+      var movie_html = "";
+
+      movie_html += "<h3>" + movie.name + "(" + movie.info.year + ")</h3>";
+      movie_html += '<img src="' + movie.info.poster + '">';
+      movie_html += "<h4>Combined Rating: " + movie.combined_rating + "</h4>";
+
+      var movie_ratings = "<h4>Ratings: </h4>";
+      $.each(movie.ratings, function(rating_site, rating) {
+        movie_ratings += "<h4>" + rating_site + ":</h4>";
+        $.each(rating, function(key, value) {
+          movie_ratings += "<p>";
+          movie_ratings += "<b>" + key + "</b>: " + value;
+        });
+      });
+      movie_html += movie_ratings;
+
+      $('#result').html(movie_html);
+    });
+    return false;
+  });
 });
