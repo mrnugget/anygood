dc3447521f4378dca332622f2922be674dd8cc69
Thorsten Ball <mrnugget@gmail.com>
2012-07-05 12:25:28 +0200

    Refactor MovieFetcher

--- lib/anygood/movie_fetcher.rb
+++ lib/anygood/movie_fetcher.rb
@@ -1,61 +1,59 @@
 module AnyGood
   class MovieFetcher
 
+    CLIENTS = [Clients::IMDB, Clients::RottenTomatoes]
+    CACHE_TTL = 14400
+
     def self.fetch_by_name_and_year(moviename, year)
       new.fetch_by_name_and_year(moviename, year)
     end
 
     def fetch_by_name_and_year(moviename, year)
-      ratings = ratings_from_cache_or_clients(moviename, year)
-      info    = info_from_cache_or_client(Clients::RottenTomatoes, moviename, year)
+      ratings = ratings_for(moviename, year)
+      info    = info_for(Clients::RottenTomatoes, moviename, year)
 
       MovieMatcher.new.incr_score_for(name: moviename, year: year)
       Movie.new(name: moviename, year: year, ratings: ratings, info: info)
     end
 
+    def initialize(cache = REDIS, clients = CLIENTS)
+      @cache   = cache
+      @clients = clients
+    end
+
     private
 
-      def clients
-        [
-          Clients::IMDB,
-          Clients::RottenTomatoes,
-        ]
+      def info_for(client, moviename, year)
+        fetch_from_cache_or_client(:info, client, moviename, year)
       end
 
-      def info_from_cache_or_client(client, moviename, year)
-        cached_info = REDIS.get(info_key_for(moviename, client.name))
-
-        unless cached_info
-          fetch_and_save_to_cache(:info, Clients::RottenTomatoes, moviename, year)
-        else
-          JSON.parse(cached_info, symbolize_names: true)
-        end
-      end
-
-      def ratings_from_cache_or_clients(moviename, year)
+      def ratings_for(moviename, year)
         ratings = {}
 
-        clients.each do |client|
-          cached_rating = REDIS.get(rating_key_for(moviename, client.name))
-
-          unless cached_rating
-            ratings[client.name] = fetch_and_save_to_cache(:rating, client, moviename, year)
-          else
-            ratings[client.name] = JSON.parse(cached_rating, symbolize_names: true)
-          end
+        @clients.each do |client|
+          ratings[client.name] = fetch_from_cache_or_client(:rating, client, moviename, year)
         end
 
         ratings
       end
 
+      def fetch_from_cache_or_client(type, client, moviename, year)
+        key = send("#{type.to_s}_key_for", moviename, client.name)
+        get_from_cache(key) || fetch_and_save_to_cache(type, client, moviename, year)
+      end
+
       def fetch_and_save_to_cache(type, client, moviename, year)
         result = client.fetch(moviename, year).send(type)
-        REDIS.setex(
-          send("#{type.to_s}_key_for", moviename, client.name),
-          14400,
-          result.to_json
-        )
+        key    = send("#{type.to_s}_key_for", moviename, client.name)
+
+        @cache.setex(key, CACHE_TTL, result.to_json)
+
         result
+      end
+
+      def get_from_cache(key)
+        cached = @cache.get(key)
+        cached ? JSON.parse(cached, symbolize_names: true) : cached
       end
 
       def info_key_for(moviename, client_name)
