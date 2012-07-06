df35c558e6c93ee2766f361e02b7bfa646dcce09
Thorsten Ball <mrnugget@gmail.com>
2012-07-06 17:24:15 +0200

    Mock MovieCache and Clients in MovieFetcher spec

--- spec/unit/movie_fetcher_spec.rb
+++ spec/unit/movie_fetcher_spec.rb
@@ -3,119 +3,111 @@ require 'spec_helper'
 describe AnyGood::MovieFetcher do
   describe '.fetch_by_name_and_year' do
     before(:each) do
-      stub_rottentomatoes_query('Inception', 'rt_inception')
-      stub_imdb_query('Inception', 2010, 'imdb_inception')
+      @client_one_rating = {score: 8.0, url: 'www.one.com'}
+      client_one_result  = stub(:client_one_result, rating: @client_one_rating )
+      @client_one        = stub(:client_one, name: 'client_one', fetch: client_one_result)
+
+      @client_two_rating = {score: 4.0, url: 'www.two.com'}
+      client_two_result  = stub(:client_two_result, rating: @client_two_rating)
+      @client_two        = stub(:client_two, name: 'client_two', fetch: client_two_result)
+
+      @movie_info        = {poster: 'www.posterurl.com/pic.jpg'}
+      info_client_result = stub(:info_client_result, info: @movie_info)
+      @info_client       = stub(:info_client, name: 'info_client', fetch: info_client_result)
+
+      @cache = stub(:cache, get: nil).as_null_object
 
       @movie_fetcher = AnyGood::MovieFetcher.new
+      @movie_fetcher.rating_clients = [@client_one, @client_two]
+      @movie_fetcher.info_client    = @info_client
+      @movie_fetcher.cache          = @cache
     end
 
-    it 'returns a Movie object with attributes fetched from different clients' do
+    it 'returns an movie object with the right attributes' do
       movie = @movie_fetcher.fetch_by_name_and_year('Inception', 2010)
 
       movie.should be_a(AnyGood::Movie)
-      movie.ratings['IMDB'][:score].should == 8.8
-      movie.ratings['Rotten Tomatoes'][:score].should == 8.95
+      movie.ratings['client_one'][:url].should == 'www.one.com'
+      movie.ratings['client_one'][:score].should eq(8.0)
+      movie.ratings['client_two'][:url].should == 'www.two.com'
+      movie.ratings['client_two'][:score].should eq(4.0)
+      movie.info[:poster].should == 'www.posterurl.com/pic.jpg'
+      movie.combined_rating.should eq(6.0)
     end
 
-    it 'returns an appropriate message if one of the clients response could not be parsed' do
-      stub_rottentomatoes_query('Inception', 'json_parse_error')
+    it 'checks the cache before fetching through the clients' do
+      @cache.should_receive(:get).exactly(3).times
 
-      movie = @movie_fetcher.fetch_by_name_and_year('Inception', 2010)
-
-      movie.ratings['Rotten Tomatoes'][:error].should == 'Could not be parsed'
+      @movie_fetcher.fetch_by_name_and_year('Inception', 2010)
     end
 
-    it 'returns an appropriate message if the info from RT could not be parsed' do
-      stub_rottentomatoes_query('Inception', 'json_parse_error')
-
-      movie = @movie_fetcher.fetch_by_name_and_year('Inception', 2010)
-
-      movie.info[:error].should == 'Could not be parsed'
-    end
-
-    describe 'caching client results' do
-      let(:encoded_movie_name) do
-        URI.encode('Inception')
-      end
-
-      let(:encoded_rt_name) do
-        URI.encode('Rotten Tomatoes')
-      end
-
-      let(:encoded_imdb_name) do
-        URI.encode('IMDB')
-      end
-
-      it 'checks the cache before fetching via clients' do
-        AnyGood::REDIS.should_receive(:get).with(
-          "movierating:#{encoded_movie_name}:#{encoded_imdb_name}"
-        )
-        AnyGood::REDIS.should_receive(:get).with(
-          "movierating:#{encoded_movie_name}:#{encoded_rt_name}"
-        )
+    context 'cache is empty' do
+      it 'fetches the movie ratings and info through the clients' do
+        @client_one.should_receive(:fetch)
+        @client_two.should_receive(:fetch)
+        @info_client.should_receive(:fetch)
 
         @movie_fetcher.fetch_by_name_and_year('Inception', 2010)
       end
 
-      context 'ratings are not in the cache' do
-        it 'saves the clients results to cache' do
-          AnyGood::REDIS.should_receive(:setex).with(
-            "movierating:#{encoded_movie_name}:#{encoded_imdb_name}",
-            14400,
-            {score: 8.8, url: 'http://www.imdb.com/title/tt1375666'}.to_json
-          )
-          AnyGood::REDIS.should_receive(:setex).with(
-            "movierating:#{encoded_movie_name}:#{encoded_rt_name}",
-            14400,
-            {score: 8.95, url: 'http://www.rottentomatoes.com/m/inception/'}.to_json
-          )
+      it 'writes the fetched client results into the cache' do
+        @cache.should_receive(:write).with(:rating, 'Inception', 'client_one', @client_one_rating)
+        @cache.should_receive(:write).with(:rating, 'Inception', 'client_two', @client_two_rating)
 
-          @movie_fetcher.fetch_by_name_and_year('Inception', 2010)
-        end
+        @cache.should_receive(:write).with(:info, 'Inception', 'info_client', @movie_info)
+
+        @movie_fetcher.fetch_by_name_and_year('Inception', 2010)
+      end
+    end
+
+    context 'cache is filled' do
+      before(:each) do
+        @cache.stub(get: :cached_result)
       end
 
-      context 'ratings are already in the cache' do
-        it 'does not fetch ratings via clients when the rating is already in cache' do
-          AnyGood::REDIS.set(
-            "movierating:#{encoded_movie_name}:#{encoded_imdb_name}",
-            {score: 9.0, url: 'example.org'}.to_json
-          )
+      it 'fetches the results from the cache' do
+        @cache.should_receive(:get).exactly(3).times
 
-          AnyGood::Clients::IMDB.should_not_receive(:fetch)
-
-          @movie_fetcher.fetch_by_name_and_year('Inception', 2010)
-        end
-
-        it 'fetches the movieinfo from RT even if the RT rating is in cache' do
-          AnyGood::REDIS.set(
-            "movierating:#{encoded_movie_name}:#{encoded_rt_name}",
-            {score: 9.0, url: 'example.org'}.to_json
-          )
-
-          AnyGood::Clients::RottenTomatoes.should_receive(:fetch).once {
-            stub(:rt).as_null_object
-          }
-
-          @movie_fetcher.fetch_by_name_and_year('Inception', 2010)
-        end
+        @movie_fetcher.fetch_by_name_and_year('Inception', 2010)
       end
 
-      context 'movieinfo is already in the cache' do
-        it 'does not fetch the movieinfo' do
-          AnyGood::REDIS.set(
-            "movieinfo:#{encoded_movie_name}:#{encoded_rt_name}",
-            {
-              poster: 'http://content8.flixster.com/movie/10/93/37/10933762_det.jpg',
-              year: 2010
-            }.to_json
-          )
+      it 'does not fetch via the clients' do
+        @client_one.should_not_receive(:fetch)
+        @client_two.should_not_receive(:fetch)
+        @info_client.should_not_receive(:fetch)
 
-          AnyGood::Clients::RottenTomatoes.should_receive(:fetch).once {
-            stub(:rt).as_null_object
-          }
+        @movie_fetcher.fetch_by_name_and_year('Inception', 2010)
+      end
+    end
 
-          @movie_fetcher.fetch_by_name_and_year('Inception', 2010)
-        end
+    context 'only the movieinfo is written to the cache' do
+      it 'still fetches the ratings via the clients' do
+        @cache.should_receive(:get).with(:info, 'Inception', 'info_client').and_return(:cached_info)
+
+        @cache.should_receive(:get).with(:rating, 'Inception', 'client_one').and_return(nil)
+        @cache.should_receive(:get).with(:rating, 'Inception', 'client_two').and_return(nil)
+
+        @client_one.should_receive(:fetch)
+        @client_two.should_receive(:fetch)
+        @info_client.should_not_receive(:fetch)
+
+        @movie_fetcher.fetch_by_name_and_year('Inception', 2010)
+      end
+    end
+
+    context 'only the ratings are written to the cache' do
+      it 'still fetches the movie info via the info client' do
+        @cache.should_receive(:get).with(:rating, 'Inception', 'client_one').and_return(:cached_result)
+        @cache.should_receive(:get).with(:rating, 'Inception', 'client_two').and_return(:cached_result)
+
+        @cache.should_receive(:get).with(:info, 'Inception', 'info_client').and_return(nil)
+
+        @info_client.should_receive(:fetch)
+
+        @client_one.should_not_receive(:fetch)
+        @client_two.should_not_receive(:fetch)
+
+        @movie_fetcher.fetch_by_name_and_year('Inception', 2010)
       end
     end
   end
