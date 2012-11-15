95a79d2f859466735e8fbde8a8a4a2732efed556
Thorsten Ball <mrnugget@gmail.com>
2012-11-15 17:53:08 +0100

    Move assumptions in one MovieImporter spec to make it faster

--- spec/unit/movie_importer_spec.rb
+++ spec/unit/movie_importer_spec.rb
@@ -10,31 +10,19 @@ describe AnyGood::MovieImporter do
       )
     end
 
-    it 'fetches a wikipedia site with a movielist for the given year' do
-      movie_importer = AnyGood::MovieImporter.new(2011)
-
-      movie_importer.fetch_movies
-
-      a_request(
-        :get, 'http://en.wikipedia.org/wiki/2011_in_film'
-      ).should have_been_made
-    end
-
-    it 'returns a list of movies from that year' do
+    it 'fetches movies from a year from wikipedia and returns a list of movies' do
       movie_importer = AnyGood::MovieImporter.new(2011)
 
       movie_list = movie_importer.fetch_movies
 
       movie_list.should include('Rango', 'The Lincoln Lawyer', 'Crazy, Stupid, Love.')
       movie_list.should_not include('Airplane!', 'West Side Story', 'Kindergarten Cop')
-    end
-
-    it 'should not contain duplicates' do
-      movie_importer = AnyGood::MovieImporter.new(2011)
-
-      movie_list = movie_importer.fetch_movies
 
       movie_list.count.should eq(movie_list.uniq.count)
+
+      a_request(
+        :get, 'http://en.wikipedia.org/wiki/2011_in_film'
+      ).should have_been_made
     end
   end
 end
