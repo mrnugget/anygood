bf8e56c0f4982265aebbeb505b4fdade09252885
Thorsten Ball <mrnugget@gmail.com>
2013-05-11 08:51:31 +0200

    Refactor movie importer

--- lib/anygood/movie_importer.rb
+++ lib/anygood/movie_importer.rb
@@ -1,29 +1,20 @@
 module AnyGood
   class MovieImporter
     def initialize(year)
-      @uri = wiki_url(year)
+      @uri = URI(URI.encode("http://en.wikipedia.org/wiki/#{year}_in_film"))
     end
 
     def fetch_movies
-      response    = Net::HTTP.get_response(@uri)
-      @movie_list = parse_wiki_page(response.body)
+      response = Net::HTTP.get_response(@uri)
+      parse_wiki_page(response.body)
     end
 
     private
 
-      def wiki_url(year)
-        URI(URI.encode("http://en.wikipedia.org/wiki/#{year}_in_film"))
-      end
-
       def parse_wiki_page(html)
-        names = []
-        doc   = Nokogiri::HTML(html)
-
-        doc.css('table.wikitable td i a').each do |node|
-          names << node.children.first.text
-        end
-
-        names.uniq
+        Nokogiri::HTML(html).css('table.wikitable td i a').map { |node|
+          node.children.first.text
+        }.uniq
       end
   end
 end
